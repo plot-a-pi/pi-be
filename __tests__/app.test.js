370faf8a818e8ba222d2be44bedaeea5391d4303
Jacob Harrington <jharrington4@gmail.com>
2020-02-27 09:23:33 -0800

    csv session route is completed and tests passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const GlobalData = require('../lib/models/GlobalData');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -18,7 +19,22 @@ describe('app routes', () => {
     return mongoose.connection.close();
   });
 
-  it('can pass travis', () => {
-    
+  it('get sesssion data by id', async() => {
+    const sessionData = [
+      { circumference: 10, diameter: 5, sessionId: 'session1', diameterUnit:'in', circumferenceUnit: 'in' },
+      { circumference: 5, diameter: 2.5, sessionId: 'session1', diameterUnit:'in', circumferenceUnit: 'in' },
+      { circumference: 2.5, diameter: 1, sessionId: 'session1', diameterUnit:'in', circumferenceUnit: 'in' },
+      { circumference: 15, diameter: 7.5, sessionId: 'session1', diameterUnit:'in', circumferenceUnit: 'in' }
+    ];
+    await GlobalData.create(sessionData);
+
+    return request(app)
+      .get('/session-data/session1')
+      .then(res => {
+
+        res.body.forEach(item => {
+          expect(res.body).toContain(item);
+        });
+      });
   });
 });
